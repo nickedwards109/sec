ed154b5a934d9fa721cb923e121e1aa2a3f80989
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-16 11:21:43 -0600

    Modify request spec for authentication via SHA-256 HMAC
    Previously, the request spec was checking for a digital signature generated by an AES encryption of the HTTP request line. Now, the request spec checks for a digital signature generated by a SHA-256 HMAC hash of the request line. This authentication scheme is more adherent to common web conventions.

--- spec/requests/api/v1/secret_messages_request_spec.rb
+++ spec/requests/api/v1/secret_messages_request_spec.rb
@@ -13,19 +13,23 @@ describe 'Secret messages API' do
     expect(response).to have_http_status(404)
   end
 
-  xit 'responds with a 200 to an authenticated request' do
+  it 'responds with an HTTP 200 response to an authenticated request' do
     # This overrides the password in application.yml which is ignored from
     #  version control
-    ENV['password'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
+    ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
 
-    # When this password is used with AES-256-CBC to encrypt the following
-    #  string: 'GET https://secret-message-server.herokuapp.com/api/v1/secret_messages/1.json 1.1'
+    # When this key is used with SHA-256 HMAC to hash the following
+    #  string: 'GET http://localhost:3000/api/v1/secret_messages/1.json 1.1'
     #  the resulting string is the digital signature below.
     # This follows the authentication scheme described in README.md.
-    # This test data is the same data used in the client-side application at
-    # https://github.com/nickedwards109/secret-message-client
-    signature = '73c9f540c5f00bc56f07b6ba5f2e1e34b5b937ed4db731a4500424458cf6a72d18ec0ad61b12a7f24f181f7815c69166ee94dfc6fa4cb5871b0d086ee7e31fccf3f720de99f9f5fb06eb770c61bde6e905eaf9f6f93548eb33c1eb7463901684'
-    get '/api/v1/secret_messages/1.json', headers: { 'Authorization' => signature }
+    # Eventually, when this app is pushed to production, the endpoint will be:
+    #  http://secret-message-server.herokuapp.com/api/v1/secret_messages/1.json
+    correct_signature = '90aa7d0c79677d86800500f6c999a6273544169f3912322c5e48174b58fb90dc'
+    get '/api/v1/secret_messages/1.json', headers: { 'Authorization' => correct_signature }
+
+    secret_message = JSON.parse(response.body)["description"]
+
+    expect(secret_message.class).to equal(String)
     expect(response).to have_http_status(200)
   end
 end
