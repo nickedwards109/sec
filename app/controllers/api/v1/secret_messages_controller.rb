10b976198f8ee486202dca689e1130703b3d578a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-16 11:25:38 -0600

    Implement authenticating HTTP requests
    This commit implements changes that authenticate incoming HTTP requests by checking for a digital signature generated by a SHA-256 HMAC hash of the HTTP request line. The digital signature must be present as the value of an 'Authorization' header.
    
    This authentication scheme ensures that the client has access to a secret key that is also known by the server (meaning they are who they say they are, assuming the key is unknown to any other parties). It also ensures that the request was not tampered with in transit between the client and server.

--- app/controllers/api/v1/secret_messages_controller.rb
+++ app/controllers/api/v1/secret_messages_controller.rb
@@ -1,9 +1,30 @@
 class Api::V1::SecretMessagesController < ApplicationController
   before_action :authenticate_request
   def show
+    render json: { description: "secret message placeholder" }
   end
 
   def authenticate_request
-    render status: 404
+    # development scheme and host:
+    scheme = 'http://'
+    host = 'localhost:3000'
+
+    # production scheme and host:
+    # scheme = 'https://'
+    # host = 'secret-message-server.herokuapp.com'
+
+    method = request.request_method
+    path = request.fullpath
+    url = scheme + host + path
+    http_version = 'HTTP/1.1'
+    request_line = method + ' ' + url + ' ' + http_version
+
+    key = ENV['key']
+    digest = OpenSSL::Digest.new('sha256')
+    expected_signature = OpenSSL::HMAC.hexdigest(digest, key, request_line)
+
+    actual_signature = request.headers['Authorization']
+
+    render status: 404 if expected_signature != actual_signature
   end
 end
