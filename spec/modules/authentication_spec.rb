902d4ba59079c6b8c5393bf6b21226d2ad8248f5
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-16 23:56:46 -0600

    Add unit specs for the Authentication module
    This commit adds unit specs for the #authenticated?(request) method of the Authentication module. In order for this method to return true, the request object must have properties of an HTTP method and a path that, when hashed with a key, will produce a hash that matches the value of the request's Authorization header.
    
    This commit also makes a couple of minor edits to comments:
      * Refer to the key as a key instead of a password
      * Show the HTTP version in the request line as HTTP/1.1 instead of just 1.1

---
+++ spec/modules/authentication_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+require './lib/authentication.rb'
+
+describe 'authentication module' do
+  include Authentication
+
+  # stub an incoming HTTP request
+  class Request
+    attr_accessor :headers, :request_method, :fullpath
+  end
+
+  before(:each) do
+    # This overrides the key in application.yml which is ignored from
+    #  version control
+    @request = Request.new
+  end
+
+  it 'authenticates a request with a valid signature' do
+    ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
+    @request.headers = { 'Authorization' => '90aa7d0c79677d86800500f6c999a6273544169f3912322c5e48174b58fb90dc' }
+    @request.request_method = 'GET'
+    @request.fullpath = '/api/v1/secret_messages/1.json'
+    expect(authenticated?(@request)).to be_truthy
+  end
+
+  it 'does not authenticate a request with an invalid signature' do
+    ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
+    @request.headers = { 'Authorization' => 'asdf plz' }
+    @request.request_method = 'GET'
+    @request.fullpath = '/api/v1/secret_messages/1.json'
+    expect(authenticated?(@request)).to be_falsy
+  end
+
+  it 'does not authenticate a request whose HTTP method or URL do not match the API requirements' do
+    ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
+
+    # The signature in these tests is a hash of the request line 'GET http://localhost:3000/api/v1/secret_messages/1.json HTTP/1.1'
+    @request.headers = { 'Authorization' => '90aa7d0c79677d86800500f6c999a6273544169f3912322c5e48174b58fb90dc' }
+
+    # incorrect HTTP method
+    @request.request_method = 'POST'
+    @request.fullpath = '/api/v1/secret_messages/1.json'
+    expect(authenticated?(@request)).to be_falsy
+
+    # incorrect URL
+    @request.request_method = 'GET'
+    @request.fullpath = '/api/v42/public_messages/1.json'
+    expect(authenticated?(@request)).to be_falsy
+  end
+
+  it 'does not authenticate a request when the key is incorrect' do
+    ENV['key'] = 'lulz0194r019RnE2plz'
+    @request.headers = { 'Authorization' => '90aa7d0c79677d86800500f6c999a6273544169f3912322c5e48174b58fb90dc' }
+    @request.request_method = 'GET'
+    @request.fullpath = '/api/v1/secret_messages/1.json'
+    expect(authenticated?(@request)).to be_falsy
+  end
+end
--- spec/requests/api/v1/secret_messages_request_spec.rb
+++ spec/requests/api/v1/secret_messages_request_spec.rb
@@ -14,12 +14,12 @@ describe 'Secret messages API' do
   end
 
   it 'responds with an HTTP 200 response to an authenticated request' do
-    # This overrides the password in application.yml which is ignored from
+    # This overrides the key in application.yml which is ignored from
     #  version control
     ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
 
     # When this key is used with SHA-256 HMAC to hash the following
-    #  string: 'GET http://localhost:3000/api/v1/secret_messages/1.json 1.1'
+    #  string: 'GET http://localhost:3000/api/v1/secret_messages/1.json HTTP/1.1'
     #  the resulting string is the digital signature below.
     # This follows the authentication scheme described in README.md.
     # Eventually, when this app is pushed to production, the endpoint will be:
