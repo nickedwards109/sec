7055dd04e9b60283fbfdaddcbfeb404b5709d81a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-22 10:56:56 -0600

    Improve testability of an encrypted HTTP response
    The prior signature of Encryption#encrypt(data, initialization_vector) would have made it difficult or impossible to test the contents of an encrypted HTTP response in the request specs. This is because #encrypt would not be called in the request spec, meaning the initialization vector could not be set in that environment, meaning a random initialization vector would be used each time and the encrypted HTTP response would be different each time. Although this is the desired production behavior, it makes it difficult to test the exact contents of an encrypted HTTP response.
    
    This commit applies changes that remove initialization_vector from the arguments of #encrypt. Instead, the initialization vector comes from an environment variable if that environment variable exists, and otherwise it is randomly generated within #encrypt. This means that both the encryption key and the initialization vector can be set in the test environment via environment variables, which will result in a deterministic and testable encrypted HTTP response. In production, there will be no environment variable for the initialization vector and it will instead be randomly generated for each response, which is preferable as per the Ruby OpenSSL docs.

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,15 +1,22 @@
 module Encryption
-  def encrypt(text, iv = SecureRandom.hex(8))
+  def encrypt(text)
     cipher = OpenSSL::Cipher::AES.new(256, :CBC)
     cipher.encrypt
     cipher.key = ENV['key']
-    cipher.iv = iv
+
+    # If this is executed via the request specs, ENV['initialization_vector']
+    #  is set in the specs.
+    # ENV['initialization_vector'] is not set in application.yml.
+    # If ENV['initialization_vector'] does not exist, generate a new random
+    #  initialization vector.
+    initialization_vector = ENV['initialization_vector'] || SecureRandom.hex(8)
+    cipher.iv = initialization_vector
     encrypted = cipher.update(text) + cipher.final
 
     # as of now, encrypted is a string of binary characters encoded as rubys's ASCII-8bit
     # convert that data into a string of hex characters
     encrypted_hex = encrypted.unpack('H*')[0]
 
-    { cipher: encrypted_hex, iv: iv }
+    { cipher: encrypted_hex, initialization_vector: initialization_vector }
   end
 end
--- spec/modules/encryption_spec.rb
+++ spec/modules/encryption_spec.rb
@@ -6,24 +6,30 @@ describe 'encryption module' do
 
   it 'encrypts data and returns a hash containing the encrypted data and the initialization vector' do
     ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
-    iv = '35cd6e2b82b6537d'
-
-    data = "There's always money in the banana stand."
-
-    result = encrypt(data, iv)
-
-    expect(result.class).to equal(Hash)
-    expect(result[:cipher]).to eql('2d8c5aa513c04092ae3811f9a7dde8286e00badeb8310907c64da9c7289ed74734f4246a8c49f088ebea3d7d154604f5')
-    expect(result[:iv]).to eql('35cd6e2b82b6537d')
-  end
-
-  it 'generates an initialization vector if one is not provided' do
-    ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
+    ENV['initialization_vector'] = '35cd6e2b82b6537d'
 
     data = "There's always money in the banana stand."
 
     result = encrypt(data)
 
-    expect(result[:iv].class).to equal(String)
+    expect(result.class).to equal(Hash)
+    expect(result[:cipher]).to eql('2d8c5aa513c04092ae3811f9a7dde8286e00badeb8310907c64da9c7289ed74734f4246a8c49f088ebea3d7d154604f5')
+    expect(result[:initialization_vector]).to eql('35cd6e2b82b6537d')
+  end
+
+  it 'generates a random initialization vector if one is not provided' do
+    ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
+    ENV['initialization_vector'] = nil
+
+    data = "There's always money in the banana stand."
+
+    result1 = encrypt(data)
+
+    expect(result1[:initialization_vector].class).to equal(String)
+    expect(result1[:initialization_vector].length).to equal(16)
+
+    result2= encrypt(data)
+
+    expect(result2).not_to eql(result1)
   end
 end
