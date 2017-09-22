9910c5ce34b9f62241b8fe651d2cc8de3a600a31
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-22 13:09:47 -0600

    Specify an encrypted HTTP response
    When an authenticated request is made for an existing secret message resource, the response should contain a body with an AES-256-CBC cipher of the requested resource, as well as a header with the initialization vector that was used to encrypt the data.

--- spec/requests/api/v1/secret_messages_request_spec.rb
+++ spec/requests/api/v1/secret_messages_request_spec.rb
@@ -32,4 +32,21 @@ describe 'Secret messages API' do
     expect(secret_message.class).to equal(String)
     expect(response).to have_http_status(200)
   end
+
+  it 'encrypts the response body with AES-256-CBC and sends the initialization vector' do
+    original_message = SecretMessage.create(message: "There's always money in the banana stand.")
+
+    ENV['key'] = 'ff5c66433974329a675b114f27799251821c6830f0aa69cbb62cfcce6c6cd20f864494873c0e513a2033f650f357ef2cc9818b6541bf5625d3a4ee374e254b1e'
+    ENV['initialization_vector'] = '35cd6e2b82b6537d'
+    correct_signature = '90aa7d0c79677d86800500f6c999a6273544169f3912322c5e48174b58fb90dc'
+    get '/api/v1/secret_messages/1.json', headers: { 'Authorization' => correct_signature }
+
+    response_initialization_vector = response.headers["initialization_vector"]
+    response_body = JSON.parse(response.body)
+    response_cipher = response["cipher"]
+
+    expected_response_cipher = "2d8c5aa513c04092ae3811f9a7dde8286e00badeb8310907c64da9c7289ed74734f4246a8c49f088ebea3d7d154604f5"
+    expect(response_cipher).to equal(expected_response_cipher)
+    expect(response_initialization_vector).to equal('35cd6e2b82b6537d')
+  end
 end
