8708af1b300340101f640fcbf45f3d439b6b1bcb
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-24 17:42:19 -0600

    Implement responding to a request for all resources
    This commit implements a response to a request for all secret message resources. The response body is a JSON string where the top-level key is the resource's id, and the corresponding value is a JSON object with key-value pairs for the encrypted message and the AES initialization vector.

--- app/controllers/api/v1/secret_messages_controller.rb
+++ app/controllers/api/v1/secret_messages_controller.rb
@@ -2,6 +2,17 @@ require './lib/encryption'
 
 class Api::V1::SecretMessagesController < AuthenticationController
   include Encryption
+
+  def index
+    secret_messages = SecretMessage.all
+    response_body = {}
+    secret_messages.each do |secret_message|
+      encryption_output = encrypt(secret_message.message)
+      response_body[secret_message.id] = encryption_output
+    end
+    render json: response_body
+  end
+
   def show
     message = SecretMessage.find(params[:id]).message
     encryption_output = encrypt(message)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :secret_messages, only: [:show]
+      resources :secret_messages, only: [:index, :show]
     end
   end
 end
